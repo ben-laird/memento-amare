91c14955f9358023c77d1493f51e8492e5e711ac
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-02-16 02:39:51 -0500

    feat(content): :sparkles: add content
    Add content to the app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+<h1
+  class="prose m-4 rounded-md bg-pink-400 p-4 text-center text-white lg:rounded-3xl lg:text-6xl"
+>
+  Welcome to Memento Amare
+</h1>
+<p class="prose m-3 mx-auto w-full p-3 text-center text-sm lg:text-2xl">
+  Hi Allie. I hope you're having a great day today. I wanted to make this app
+  for you to show you how much I love you. For as long as the internet is
+  available, this site will be running, hosted on some server, and it will
+  proclaim one simple thing:
+</p>
+
+<h2
+  class="prose m-2 rounded-full bg-red-600 p-2 text-center text-xl text-white lg:m-4 lg:p-4 lg:text-7xl"
+>
+  I Love You!
+</h2>
+
+<p class="prose m-3 mx-auto w-full p-3 text-center text-sm lg:text-2xl">
+  Click <a href="/roadmap">here</a> to see what else I have planned for this project...
 </p>
---
+++ src/routes/roadmap/+page.svelte
@@ -0,0 +1,25 @@
+<h1
+  class="prose m-2 mx-4 rounded-xl border-2 border-pink-400 p-2 text-center text-xl lg:text-6xl"
+>
+  Roadmap
+</h1>
+<p class="prose mx-auto my-2 rounded-xl border-2 border-pink-400 p-2 text-xl">
+  Here's what I have planned:
+</p>
+<ul class="container prose mx-auto my-2 rounded-xl border-2 border-red-400">
+  <li class="p-2 text-xl">
+    <strong>Supabase: </strong> We can create posts that the other can see and protect
+    them with authentication. No one can see that nasty pp pic ever again lol
+  </li>
+  <li class="p-2 text-xl">
+    <strong>MDsveX: </strong> Add Markdown support to ease all this writing.
+  </li>
+  <li class="p-2 text-xl">
+    <strong>Email: </strong> Regular email reminders from the Supabase db that remind
+    us of what we've told each other. Easily get a pick-me-up or hilarious joke right
+    in your inbox!
+  </li>
+</ul>
+<p class="prose mx-auto my-2 rounded-xl border-2 border-pink-400 p-2 text-xl">
+  Go back <a href="/">home</a>
+</p>
